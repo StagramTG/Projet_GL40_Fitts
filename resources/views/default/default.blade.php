5fcaed695f4982d37729f84475cf9a7a1ffc6aae
Thomas Gredin <t.gredin@gmx.fr>
2017-05-29 16:11:39 +0200

    Ajout du template default

---
+++ resources/views/default/default.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+</head>
+<body>
+    {{-- Header et NavBar --}}
+
+    @yield('content')
+
+    {{-- Footer --}}
+</body>
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,1 +1,9 @@
-<h1>Fitts</h1>
+@extends('default/default')
+
+@section('title')
+    Accueil
+@endsection
+
+@section('content')
+    <h1>Accueil Fitts</h1>
+@endsection
