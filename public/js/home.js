4b77d893ab945ff0b62e663280d49b8ab05c79a4
Thomas Gredin <t.gredin@gmx.fr>
2017-05-30 11:36:12 +0200

    Code de rendu des boutons js

--- public/js/home.js
+++ public/js/home.js
@@ -7,7 +7,7 @@
  * @param {*taux de bleu} b 
  * @param {*alpha} a 
  */
-function Color(r, g, b, a)
+function Color(r, g, b, a = 255)
 {
     this.r = r;
     this.g = g;
@@ -23,7 +23,7 @@ function Color(r, g, b, a)
  * @param {*couleur du texte} fg_color
  * @param {*couleur de fond} bg_color
  */
-function button(x, y, text, fg_color, bg_color)
+function Button(x, y, text, fg_color, bg_color)
 {
     this.x = x;
     this.y = y;
@@ -36,12 +36,23 @@ function button(x, y, text, fg_color, bg_color)
      */
     this.update = function()
     {
+    }
 
+    /**
+     * Permet de dessiner le bouton
+     */
+    this.draw = function()
+    {
+        fill(this.bg_color.r, this.bg_color.g, this.bg_color.b);
+        stroke(this.bg_color.r, this.bg_color.g, this.bg_color.b);
+        rect(this.x, this.y, 200, 75, 20);
     }
 }
 
 // Variables globales
 var canvas;
+
+var fittsTestButton;
 
 // Mise en place du canvas et autres
 function setup()
@@ -49,10 +60,13 @@ function setup()
     canvas = createCanvas(400, 100);
     canvas.parent('test_choice');
     canvas.background(236);
+
+    fittsTestButton = new Button(10, 10, "FITTS", new Color(0, 0, 0), new Color(0, 150, 255));
 }
 
 // Chaque frame
 function draw()
 {
-    
+    smooth();
+    fittsTestButton.draw();
 }
