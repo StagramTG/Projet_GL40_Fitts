26ea5e27e0edc99a05518061da2b569aa0940870
Thomas Gredin <t.gredin@gmx.fr>
2017-05-30 10:26:59 +0200

    Ajout du controller Pages

---
+++ app/Http/Controllers/Pages.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Pages extends Controller
+{
+    /*
+        Constructeur
+    */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /*
+        Page d'accueil du site
+    */
+    public function home()
+    {
+
+    }
+}
