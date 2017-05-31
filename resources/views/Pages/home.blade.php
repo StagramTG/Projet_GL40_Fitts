e311f277abc9d69a718d052eeea8af8784011a4a
Thomas Gredin <t.gredin@gmx.fr>
2017-05-31 14:17:27 +0200

    Ajout text dans la page d'accueil

--- resources/views/Pages/home.blade.php
+++ resources/views/Pages/home.blade.php
@@ -8,6 +8,12 @@
     {{-- Explications du concept du site --}}
 
     {{-- Choix des différents tests --}}
+    <h2>Différents tests disponibles</h2>
+    <p>
+        Veuillez retrouver ci-dessous les différents tests proposés pour les lois en lien avec
+        les IHM.
+    </p>
+
     <div class="test-choices">
         {{-- Ici on met les boutons pour les choix de tests --}}
         <div class="btn-fitts btn-test">
