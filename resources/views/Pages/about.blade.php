0eec37d616a70b211c4b6fa8d3142b1a2f168b91
Thomas Gredin <t.gredin@gmx.fr>
2017-06-02 11:12:07 +0200

    Début rédaction page about

--- resources/views/Pages/about.blade.php
+++ resources/views/Pages/about.blade.php
@@ -4,6 +4,22 @@
 <div class="content">
 
     <a href="{{ url('home') }}">Retour à l'accueil</a>
+    <h1>
+        <span class="big-title">A propos de ce site</span>
+    </h1>
+
+    <p>
+        Ce site est né dans le cadre de la réalisation d'un projet de l'UV GL40 () par trois étudiants
+        de l'UTBM :
+
+        <ul>
+            <li>Rémi Ponnelle</li>
+            <li>Emilien Moncan</li>
+            <li>Thomas Gredin</li>
+        </ul>
+
+
+    </p>
 
 </div>
 @endsection
