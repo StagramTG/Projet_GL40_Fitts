c6a9b8d06f0e741f7729939d290ccbc1aa853cee
Thomas Gredin <t.gredin@gmx.fr>
2017-06-05 22:09:58 +0200

    Fin de session

--- public/js/fitts/app.js
+++ public/js/fitts/app.js
@@ -17,10 +17,17 @@ var container;
 function setup()
 {
     container = document.getElementById('canvas');
+    let cw = container.offsetWidth;
 
-    p5Canvas = createCanvas(960, 480);
+    p5Canvas = createCanvas(cw, cw / (16/9));
     p5Canvas.background(0);
     p5Canvas.parent('canvas');
+
+    window.addEventListener('resize', function()
+    {
+        let cw = container.offsetWidth;
+        p5Canvas.setSize(cw, cw / (16/9));
+    });
 }
 
 /**
