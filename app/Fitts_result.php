ffd8f9ee811f16ff7d1a39cbfc639069d77818df
Thomas Gredin <t.gredin@gmx.fr>
2017-06-13 21:14:51 +0200

    Remplissage du modèle fitts_result

--- app/Fitts_result.php
+++ app/Fitts_result.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fitts_result extends Model
 {
-    //
+    /* La table concernée */
+    protected $table = 'fitts_results';
+    /* On précise qu'il n'y a pas de timestamp dans la table */
+    protected $timestamp = false;
+
+    /* Champ valable pour assignation */
+    protected $fillable = ['theorical_result', 'practical_result'];
 }
