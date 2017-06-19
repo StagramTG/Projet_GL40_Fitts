4da51e80bfad6766187d42ac56cdb14de848da0d
Thomas Gredin <t.gredin@gmx.fr>
2017-06-19 11:29:06 +0200

    Début du travail sur l'affichage des stats de fitts

--- public/js/stats/app.js
+++ public/js/stats/app.js
@@ -1,42 +1,60 @@
-var ctx = document.querySelector(".fitts_chart").getContext('2d');
+var context = document.querySelector(".fitts_chart").getContext('2d');
+var fittsChart;
 
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        labels: ["0.1 s", "0.2 s", "0.3 s", "0.4 s", "0.5 s", "0.6 s", "0.7 s", "0.8 s", "0.9 s", "1.0 s"],
         datasets: [{
-            label: '# of Votes',
-            type: 'line',
-            data: [0, 1],
-            xAxisID: 'Temps',
-            yAxisID: 'Taille',
+            label: 'Nombre de cibles',
+            type: 'bar',
+            data: [0, 10, 15, 13, 16, 11, 15],
 
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
                 'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
             ],
             borderWidth: 1
         }]
     },
+
     options: {
         scales: {
+            xAxes: [{
+                stacked: true
+            }],
             yAxes: [{
+                stacked: true,
                 ticks: {
                     beginAtZero:true
                 }
             }]
         }
     }
+});
+
+/** Fonction pour attendre la fin du chargement de la page */
+$(document).ready(function()
+{
+    /** Récupération des données par le biais d'AJAX */
+
+    /** Création du DATASET pour Chart.js */
+
+    /** Création des OPTIONS pour Chart.js */
+
+    /** Création du graphe */
 });
