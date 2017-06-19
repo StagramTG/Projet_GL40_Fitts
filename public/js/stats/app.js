6578745091b6c76880e5f74d6dc14ab2dc376808
Thomas Gredin <t.gredin@gmx.fr>
2017-06-19 12:42:14 +0200

    Mise en place AJAX stats test Fitts

--- public/js/stats/app.js
+++ public/js/stats/app.js
@@ -1,16 +1,55 @@
-var context = document.querySelector(".fitts_chart").getContext('2d');
+var contextFitts = document.querySelector(".fitts_chart").getContext('2d');
 var chartFitts;
-var chartDatas;
-var chartOptions
+var chartDatasFitts;
+var chartOptionsFitts;
 
 /** Fonction pour attendre la fin du chargement de la page */
 $(document).ready(function()
 {
     /** Récupération des données par le biais d'AJAX */
 
-    /** Création du DATASET pour Chart.js */
+    /** 
+     * Surtout ne pas oublier de renseigner le jeton 
+     * sinon ça ne fonctionne pas...
+     */
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': crsf_tocken
+        }
+    });
 
-    /** Création des OPTIONS pour Chart.js */
+    $.ajax({
+        method: 'GET',
+        url: url,
+        data: json,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(datas)
+        {
+            /** Création du DATASET pour Chart.js */
+            chartDatasFitts = {
 
-    /** Création du graphe */
+            };
+
+            /** Création des OPTIONS pour Chart.js */
+            chartOptionsFitts = {
+
+            };
+
+            /** Création de l'objet pour le graphe */
+            let chartParamsFitts = {
+                type: 'bar',
+                data: chartDatasFitts,
+                options: chartOptionsFitts
+            };
+
+            /** Création du graphe */
+            chartFitts = new Chart(contextFitts, chartParamsFitts);
+        },
+        error: function (e) 
+        {
+            alert('Echec de la récupération des données du test de Fitts');
+            document.querySelector('.ajax-result').innerHTML = e.responseText;
+        }
+    });
 });
